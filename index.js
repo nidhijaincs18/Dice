f83cbe5625cb50e854f266411eaec0b0151c2c7e
Nidhi Jain <48116222+nidhijaincs18@users.noreply.github.com>
2020-08-22 22:13:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumberString1 = "images/dice"+randomNumber1+".png";
+document.querySelector(".img1").setAttribute("src",randomNumberString1);
+var randomNumber2= Math.floor(Math.random()*6)+1;
+var randomNumberString2 = "images/dice"+randomNumber2+".png";
+document.querySelector(".img2").setAttribute("src",randomNumberString2);
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="🚩Player 1 Wins";
+}
+else if(randomNumber1< randomNumber2){
+  document.querySelector("h1").innerHTML="Player 2 Wins🚩";
+}
+else{
+  document.querySelector("h1").innerHTML="Draw!";
+}
